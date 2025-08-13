c1d54d11c7d1ebf93c98840f8ab19664265b3e8f
Yogesh <yrajak9@gmail.com>
2025-08-13 16:34:18 +0530

    model removed

--- server/graphql/user-resolvers.js
+++ server/graphql/user-resolvers.js
@@ -6,7 +6,12 @@ const uploadToCloudinary = require("../utils/cloudinary"); // adjust path as nee
 const admin = require("firebase-admin");
 
 if (!admin.apps.length) {
-  const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON);
+  const serviceAccount = JSON.parse(
+    Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT_B64, "base64").toString(
+      "utf-8"
+    )
+  );
+
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
